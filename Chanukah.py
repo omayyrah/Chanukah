6be6252dd7bad4fb6809daa6ef4b2c399a433f22
omayyrah <91054735+omayyrah@users.noreply.github.com>
2021-11-11 18:43:41 +0800

    Update Chanukah.py

--- Chanukah.py
+++ Chanukah.py
@@ -1,11 +1,13 @@
-def noOfCandles(specs):
+def noCandles(specs):
+    
     candle = specs[0]
     days = specs[1]
     
-    neededCandles = days + ((days * (days + 1))/2)
-    print('{0} {1}'.format(candle, int(neededCandles)))
+    Candles = days + ((days * (days + 1))/2)
+    print('{0} {1}'.format(candle, int(Candles)))
 
 n = int(input())
 for i in range(n):
+    
     _specs = [int(j) for j in input().split()]
-    noOfCandles(_specs)
+    noCandles(_specs)
